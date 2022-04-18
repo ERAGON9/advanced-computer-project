13c624207d339d1beae9487e1b0ffd77e1b35c92
Lior <liorbarak99@gmail.com>
2022-04-18 21:33:33 +0300

    Initial commit source files
    Initial commit only source files

---
+++ Project_Coding_Musicians_2022/Main.c
@@ -0,0 +1,26 @@
+/*
+Students:   Lior Barak   206631418
+			Shalev Kedar 314714080
+Project Coding Musicians
+
+*/
+
+#include "Concert_header.h"
+
+void main()
+{
+    FILE* ptr;
+    InstrumentTree instruments;
+    int instCount;
+    ptr = fopen("TestyMctestface.txt", "r");
+
+    if (ptr == NULL) 
+    {
+        printf("file can't be opened \n");
+    }
+
+    instruments = buildInstrumentsTree(ptr, &instCount);
+
+    // freeAll();
+	fclose(ptr);
+}
