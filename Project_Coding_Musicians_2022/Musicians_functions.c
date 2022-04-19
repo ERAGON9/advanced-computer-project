32eb9fc171ba5f98ad608c9b5a191aa1648418fb
sk-main <88608892+sk-main@users.noreply.github.com>
2022-04-19 12:53:21 +0300

    update8

--- Project_Coding_Musicians_2022/Musicians_functions.c
+++ Project_Coding_Musicians_2022/Musicians_functions.c
@@ -88,6 +88,12 @@ bool isInstrument(char* string)
 
 //task 4.........
 
+
+
+//This function create an array of arrays, each of them contain a pointer to a musician, while 
+//the number of arrays equals the number of available instruments (from the instrumenTree).
+//Each array's index in the bigger final array equals a instrument's id, and includes the pointers 
+//To all the musicians which can use that instrument.
 Musician*** constructMCollection(int iSize, Musician** group, int mSize) 
 {
 	int logSize, phySize;
@@ -113,6 +119,9 @@ Musician*** constructMCollection(int iSize, Musician** group, int mSize)
 	return res;
 }
 
+//This function check if a given instrument's id ('id') matches one of the given musician's 
+//instruments' ids, if it does a pointer to that musician will be added to the given array of
+//pointers to musicians ('arr').
 void checkMusician(Musician* player, Musician** arr, int id, int* lSize, int* pSize) 
 {
 	MPIListNode* curr = player->instruments.head;
--- Project_Coding_Musicians_2022/Musicians_header.h
+++ Project_Coding_Musicians_2022/Musicians_header.h
@@ -32,15 +32,5 @@ typedef struct
 
 } Musician;
 
-//This function create an array of arrays, each of them contain a pointer to a musician, while 
-//the number of arrays equals the number of available instruments (from the instrumenTree).
-//Each array's index in the bigger final array equals a instrument's id, and includes the pointers 
-//To all the musicians which can use that instrument.
-Musician*** constructMCollection(int iSize, Musician* group, int mSize);
-
-//This function check if a given instrument's id ('id') matches one of the given musician's 
-//instruments' ids, if it does a pointer to that musician will be added to the given array of
-//pointers to musicians ('arr').
-void checkMusician(Musician player, Musician** arr, int id, int* lSize, int* pSize);
-
-
+Musician*** constructMCollection(int iSize, Musician** group, int mSize);
+void checkMusician(Musician* player, Musician** arr, int id, int* lSize, int* pSize);
